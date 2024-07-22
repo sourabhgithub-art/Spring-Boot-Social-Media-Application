c074180024d331e6eb44821fc09aa2a01e032d81
sourabhgithub-art <choureysourabh06@gmail.com>
2024-07-22 23:30:33 +0530

    Application push to git on 07-22. for project developed during final year.

---
+++ src/main/java/com/example/questionapp/configuration/SecurityConfiguration.java
@@ -0,0 +1,86 @@
+package com.example.questionapp.configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import com.example.questionapp.security.JWTAuthenticationEntryPoint;
+import com.example.questionapp.security.JWTAuthenticationFilter;
+import com.example.questionapp.services.UserDetailsServiceImplementation;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration{                                                                 //security package da oluşturduğumuz classların hepsinin otomatik şekilde oluşmasını sağlıcak bir configuration classı oluşturdum
+
+    private UserDetailsServiceImplementation userDetailsService;
+
+    private JWTAuthenticationEntryPoint handler;                                                     //exception olduğunda handler eder
+
+    public SecurityConfiguration(UserDetailsServiceImplementation userDetailsService, JWTAuthenticationEntryPoint handler) {
+        this.userDetailsService = userDetailsService;
+        this.handler = handler;
+    }
+
+    @Bean
+    public JWTAuthenticationFilter JWTAuthenticationFilter() {
+        return new JWTAuthenticationFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors()
+                .and()
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(handler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/posts").permitAll()
+                .requestMatchers(HttpMethod.GET,"/comments").permitAll()
+                .anyRequest().authenticated();
+
+        httpSecurity.addFilterBefore(JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+}
